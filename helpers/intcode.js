31dd320f4482f0e23b59d2b5a92ac88d51868eb6
Soodonym <soodonym.alias@gmail.com>
2023-01-09 20:52:12 -0800

    2019 day 9

--- helpers/intcode.js
+++ helpers/intcode.js
@@ -4,16 +4,20 @@ class IntCode {
     for (let s of code_string.split(","))
       this.code.push(parseInt(s));
     this.step = 0;
+    this.relative_base = 0;
     this.inputs = [];
 
     this.op_map = new Map();
-    this.op_map.set(1, { func: this.add, jump: 4, args: 2 });
-    this.op_map.set(2, { func: this.mul, jump: 4, args: 2 });
-    this.op_map.set(3, { func: this.input, jump : 2, args: 0});
-    this.op_map.set(5, { func: this.jT, jump : 3, args: 2});
-    this.op_map.set(6, { func: this.jF, jump : 3, args: 2});
-    this.op_map.set(7, { func: this.lt, jump : 4, args: 2});
-    this.op_map.set(8, { func: this.eq, jump : 4, args: 2});
+    this.op_map.set(1, { func: this.add, args: 3 });
+    this.op_map.set(2, { func: this.mul, args: 3 });
+    this.op_map.set(3, { func: this.input, args: 1});
+    this.op_map.set(4, { func: this.output, args: 1});
+    this.op_map.set(5, { func: this.jT, args: 2});
+    this.op_map.set(6, { func: this.jF, args: 2});
+    this.op_map.set(7, { func: this.lt, args: 3});
+    this.op_map.set(8, { func: this.eq, args: 3});
+    this.op_map.set(9, { func: this.relative_base_update, args: 1});
+    this.op_map.set(99, { func: () => {}, args: 0});
   }
 
   addInput(i) {
@@ -26,63 +30,69 @@ class IntCode {
       let mode = modes % 10;
       modes = Math.floor(modes / 10);
       if (mode == 0)
-        values.push(this.code[this.code[this.step+1+i]]);
-      else if (mode == 1)
         values.push(this.code[this.step+1+i]);
+      else if (mode == 1)
+        values.push(this.step+1+i);
+      else if (mode == 2)
+        values.push(this.relative_base + this.code[this.step+1+i]);
     }
     return values;
   }
 
   add(values) {
-    this.code[this.code[this.step+3]] = values[0] + values[1];
+    this.code[values[2]] = this.code[values[0]] + this.code[values[1]];
   }
 
   mul(values) {
-    this.code[this.code[this.step+3]] = values[0] * values[1];
+    this.code[values[2]] = this.code[values[0]] * this.code[values[1]];
   }
 
   input(values) {
-    this.code[this.code[this.step+1]] = this.inputs.shift();
+    this.code[values[0]] = this.inputs.shift();
+  }
+
+  output(values) {
+    return this.code[values[0]];
   }
 
   jT(values) {
-    if (values[0] != 0) {
-      this.step = values[1];
+    if (this.code[values[0]] != 0) {
+      this.step = this.code[values[1]];
       return "didjump";
     }
   }
 
   jF(values) {
-    if (values[0] == 0) {
-      this.step = values[1];
+    if (this.code[values[0]] == 0) {
+      this.step = this.code[values[1]];
       return "didjump";
     }
   }
 
   lt(values) {
-    this.code[this.code[this.step+3]] = values[0] < values[1] ? 1 : 0;
+    this.code[values[2]] = this.code[values[0]] < this.code[values[1]] ? 1 : 0;
   }
 
   eq(values) {
-    this.code[this.code[this.step+3]] = values[0] == values[1] ? 1 : 0;
+    this.code[values[2]] = this.code[values[0]] == this.code[values[1]] ? 1 : 0;
+  }
+
+  relative_base_update(values) {
+    this.relative_base += this.code[values[0]];
   }
 
   exec() {
     while (true) {
-      let cmd = this.code[this.step];
-      if (cmd == 99) // halt
-        return undefined;
-      if (cmd % 100 == 4) { // output
-        let result = this.code[this.code[this.step+1]];
-        this.step += 2;
-        return result;
-      }
-      let op = this.op_map.get(cmd % 100);
-      let modes = Math.floor(cmd / 100);
+      let c = this.code[this.step];
+      let modes = Math.floor(c / 100);
+      let cmd = c % 100;
+      let op = this.op_map.get(cmd);
       let values = this.getValues(modes, op.args);
       let result = op.func.call(this, values);
       if (result != "didjump")
-        this.step += op.jump;
+        this.step += op.args + 1;
+      if (cmd == 4 || cmd == 99) // output
+        return result;
     }
   }
 }
